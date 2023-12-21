f6550ae0c18d901a336e26bc7cc8c01d32f071c4
Marina Lencinas <marinaalp@gmail.com>
2023-12-21 01:32:29 -0300

    tp final

---
+++ final_java_23557/src/main/java/modelo/Orador.java
@@ -0,0 +1,64 @@
+
+package modelo;
+
+import java.sql.Date;
+
+
+public class Orador {
+    private int idOrador;
+    private String nombre;
+    private String apellido;
+    private String mail;
+    private String tema;
+    private Date fechaAlta;
+
+    public int getIdOrador() {
+        return idOrador;
+    }
+
+    public void setIdOrador(int idOrador) {
+        this.idOrador = idOrador;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getApellido() {
+        return apellido;
+    }
+
+    public void setApellido(String apellido) {
+        this.apellido = apellido;
+    }
+
+    public String getMail() {
+        return mail;
+    }
+
+    public void setMail(String mail) {
+        this.mail = mail;
+    }
+
+    public String getTema() {
+        return tema;
+    }
+
+    public void setTema(String tema) {
+        this.tema = tema;
+    }
+
+    public Date getFechaAlta() {
+        return fechaAlta;
+    }
+
+    public void setFechaAlta(Date fechaAlta) {
+        this.fechaAlta = fechaAlta;
+    }
+    
+    
+}
